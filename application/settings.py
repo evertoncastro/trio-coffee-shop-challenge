3b284f371e7581299f08eb57eb9df7abbc010caf
Everton Castro <contato@evertoncastro.com>
2023-06-19 09:01:31 -0300

    Model basic crud tests

--- application/settings.py
+++ application/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+
+    # Custom Apps
+    'application.store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
--- application/store/models.py
+++ application/store/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+class ProductVariation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    active = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    LOCATION_CHOICES = [
+        ('in_house', 'In House'),
+        ('take_away', 'Take Away'),
+    ]
+    STATUS_CHOICES = [
+        ('waiting', 'Waiting'),
+        ('preparation', 'Preparation'),
+        ('ready', 'Ready'),
+        ('delivered', 'Delivered'),
+    ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    location = models.CharField(max_length=20, choices=LOCATION_CHOICES)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='waiting')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_variation = models.ForeignKey(ProductVariation, on_delete=models.CASCADE)
