3796e059e9c97421647d3078aadc8f0f74b5c207
Everton Castro <contato@evertoncastro.com>
2023-06-19 08:42:43 -0300

    Setup swagger docs

---
+++ application/utils.py
@@ -0,0 +1,8 @@
+from rest_framework_jwt.utils import jwt_payload_handler
+
+def jwt_response_payload_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        'user_id': user.id,
+        'email': user.email,
+    }
